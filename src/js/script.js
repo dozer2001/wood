fa4f97c6c459d7303766211bdd4dca9b99db55e1
Dozer2001 <dozer2001@mail.ru>
2020-07-02 00:28:50 +0300

    'karusels'

---
+++ src/js/script.js
@@ -0,0 +1,20 @@
+$('.slider__big').slick({
+    dots: false,
+    arrows: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    fade: true,
+    asNavFor:'.slider'
+});
+$('.slider').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    asNavFor:'.slider__big'
+});
