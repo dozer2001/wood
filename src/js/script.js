8848c45d4b024a7adf17a0a41cdf35fcd3b6fa00
Dozer2001 <dozer2001@mail.ru>
2020-07-09 13:38:29 +0300

    header_media

--- src/js/script.js
+++ src/js/script.js
@@ -32,3 +32,45 @@ $('.slider').slick({
         });
         accordionBlock[i].style.display = 'none';
     }
+
+
+var Menu = {
+
+    el: {
+        ham: $('.menu'),
+        menuTop: $('.menu-top'),
+        menuMiddle: $('.menu-middle'),
+        menuBottom: $('.menu-bottom')
+    },
+
+    init: function() {
+        Menu.bindUIactions();
+    },
+
+    bindUIactions: function() {
+        Menu.el.ham
+            .on(
+                'click',
+                function(event) {
+                    Menu.activateMenu(event);
+                    event.preventDefault();
+                }
+            );
+    },
+
+    activateMenu: function() {
+        Menu.el.menuTop.toggleClass('menu-top-click');
+        Menu.el.menuMiddle.toggleClass('menu-middle-click');
+        Menu.el.menuBottom.toggleClass('menu-bottom-click');
+    }
+};
+
+Menu.init();
+
+//Burger mune
+document.getElementById("menu").onclick = function() {open()};
+
+function open() {
+    document.getElementById("sidebar").classList.toggle("show");
+}
+// Burger mune End
